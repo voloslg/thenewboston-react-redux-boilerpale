96d2db760f636b896f93b19cec7b12ea9d5b221d
andrey <volosgoto@yahoo.com>
2019-03-25 22:28:10 +0200

    create store and userreducer

--- src/index.js
+++ src/index.js
@@ -2,4 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+import { createStore, applyMiddleware } from 'redux';
+import allReducers from './reducers';
+
+
+const store = createStore(
+  allReducers,
+  applyMiddleware(thunk, promise, logger)
+);
+
+
 ReactDOM.render(<App />, document.getElementById("root"));
