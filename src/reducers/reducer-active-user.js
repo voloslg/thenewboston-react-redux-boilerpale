cd145bfe302b3a137596dce2f400953e5158474d
volosgoto <volosgoto@yahoo.com>
2019-03-27 11:22:36 +0200

    fix reducer bug

--- src/reducers/reducer-active-user.js
+++ src/reducers/reducer-active-user.js
@@ -1,10 +1,7 @@
 export default function(state = null, action) {
   switch (action.type) {
     case "USER_SELECTED":
-      return {
-        ...state,
-        activeUser: action.payload
-      };
+      return action.payload;
 
     default:
       return state;
